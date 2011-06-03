54f62b391da145fb72e71d9701457bbad42966d9
Maciek Albin <madmck@gmail.com>
2011-06-03 21:50:41 +0100

    Decoupled socket.io

--- game/app.js
+++ game/app.js
@@ -1,22 +1,17 @@
-// Note, the includes are a mess now...
 require('./helpers.js');
-var sys = require('sys');
-
-var data = require('./data.js');
-
-// Express init
-
-var express = require('express');
+var auth = require('./auth.js')
+  , data = require('./data.js')
+  , express = require('express')
+  , forms = require('./models/forms.js')
+  , models = require('./models/models.js')
+//  , route = require('./controllers/route.js')
+  , session_store = new express.session.MemoryStore()
+  , sys = require('sys')
+  , socket = require('./socket.js');
+  
 app = module.exports = express.createServer();
 
-var io = require('socket.io')
-
-var connect = require('connect'),
-    MemoryStore = express.session.MemoryStore;
-    session_store = new MemoryStore();
-
 // App Configuration
-
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
@@ -37,12 +32,11 @@ app.configure('production', function(){
   app.use(express.errorHandler()); 
 });
 
-
 app.dynamicHelpers({
   session: function(req, res){
     return req.session;
   }
-})
+});
 
 
 // Routing information
@@ -51,83 +45,4 @@ require('./controllers/route.js');
 app.listen(3000);
 console.log("Express server listening on port %d", app.address().port);
 
-
-
-
-
-// ----------- Socket.IO Stuff ---------------- //
-
-
-var io = io.listen(app, {
-  transports: ['websocket', 'htmlfile', 'xhr-multipart', 'xhr-polling','jsonp-polling']
-});
-
-
-io.on('connection', function(client){ 
-  
-  console.log("lol wat");
-  
-  var cookie_string = client.request.headers.cookie;
-  var parsed_cookies = connect.utils.parseCookie(cookie_string);
-  var connect_sid = parsed_cookies['connect.sid'];  
-  
-  if (connect_sid) {
-    session_store.get(connect_sid, function (error, session) {
-      if(error){
-        console.log("ERROR FETCHING SESSION!");
-      }
-      //console.log(sys.inspect(session));
-      
-      //console.log(players);
-      
-      var u = session.user.username;
-      
-      // Add current player to playing users.
-      if(!data.players[u])
-        data.players[u] = session.user;
-      
-      console.log(data.players);
-      
-      // Synchronize the client
-      
-      client.send(
-          { 
-            type:     'onNewConnect',
-            me:       session.user,
-            everyone: data.players
-          }
-      );
-      
-      
-      // Tell everyone a new guy arrived
-      client.broadcast({
-        type: 'newArrival',
-        player: data.players[u]
-      })
-      
-    });
-  }
-  
-  client.on('message', function(msg){
-    console.log("message received!");
-    
-    switch(msg.type){
-      case 'playerUpdate':
-        updatePlayerArray(msg);
-        client.broadcast(msg);
-        break;
-    }
-    
-  })
-  
-  client.on('disconnect', function(){
-    console.log("person disconnected!");
-  })
-  
-});
-
-function updatePlayerArray(msg){
-  var p = data.players[msg.name];
-  p.position = msg.pos;
-  p.rotation = msg.rot;
-}
+socket.start(app,session_store,data);
---
+++ game/socket.js
@@ -0,0 +1,76 @@
+function start(server, session_store, data) {
+  var connect = require('connect')
+    , io = require('socket.io');
+    
+  io = io.listen(server, {
+    transports: ['websocket', 'htmlfile', 'xhr-multipart', 'xhr-polling','jsonp-polling']
+  });
+  
+  io.on('connection', function(client){
+    console.log("lol wat");
+  
+    var cookie_string = client.request.headers.cookie;
+    var parsed_cookies = connect.utils.parseCookie(cookie_string);
+    var connect_sid = parsed_cookies['connect.sid'];  
+  
+    if (connect_sid) {
+      session_store.get(connect_sid, function (error, session) {
+        if(error){
+          console.log("ERROR FETCHING SESSION!");
+        }
+        //console.log(sys.inspect(session));
+      
+        //console.log(players);
+      
+        var u = session.user.username;
+      
+        // Add current player to playing users.
+        if(!data.players[u])
+          data.players[u] = session.user;
+      
+        console.log(data.players);
+      
+        // Synchronize the client
+      
+        client.send({ 
+              type:     'onNewConnect',
+              me:       session.user,
+              everyone: data.players
+            });
+      
+      
+        // Tell everyone a new guy arrived
+        client.broadcast({
+          type: 'newArrival',
+          player: data.players[u]
+        });
+      
+      });
+    } else {
+      //MASSIVE ERROR
+    }
+  
+    client.on('message', function(msg){
+      console.log("message received!");
+    
+      switch(msg.type){
+        case 'playerUpdate':
+          updatePlayerArray(msg, data.players);
+          client.broadcast(msg);
+          break;
+      }
+    });
+  
+    client.on('disconnect', function(){
+      console.log("person disconnected!");
+    });
+  });
+  
+  function updatePlayerArray(msg){
+    var p = data.players[msg.name];
+    p.position = msg.pos;
+    p.rotation = msg.rot;
+  }
+}
+
+exports.start = start;
