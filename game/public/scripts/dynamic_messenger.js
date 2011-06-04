b463dbf64b0f724f272ea6ad599be73f48086078
Rafal Szymanski <rs@vlixter.com>
2011-06-04 05:12:09 +0100

    Wow, it's past 5 am, but I managed to nicely decouple and modularize the client side javascript.
    From now on, please use the same CommonJS-like namespace model
    provided by the new namespace thingy I put into /lib
    
    For documentation on it, check out https://github.com/mckoss/namespace.
    
    I actually wanted to do the live 'you got a new message' thingy,
    but got distracted by perfecting namespaces.
    I'll continue on making a live 'you got a new message' box tomorrow.

---
+++ game/public/scripts/dynamic_messenger.js
@@ -0,0 +1,10 @@
+// $(function(){
+//    $("#notification").dialog({
+//      position: 'right',
+//      height: 100,
+//      width: 170,
+//      title: "New Message!",
+//      resizable: false,
+//      draggable: false
+//    });
+//  });
--- game/public/scripts/socket_handler.js
+++ game/public/scripts/socket_handler.js
@@ -1,41 +1,70 @@
-socket = new io.Socket(null, {port: 3000, rememberTransport: false});
-socket.connect();
-
-var socket_events = {
-  'connect' : connectHandler
- ,'disconnect' : disconnectHandler
- ,'reconnect' : reconnectHandler
- ,'reconnecting' : reconnectingHandler
- ,'reconnect_failed' : reconnect_failedHandler
- ,'message' : messageHandler
-}
- 
-Object.keys(socket_events).forEach( function(event, index, arr){
-  var f = socket_events[event];
-  if(!f) console.log("Cannot find " + fname + 'function. Please create it!' );
-  else socket.on(event,f);
-})
-
-function connectHandler(){
-  console.log("i've connected!!");
-}
-
-function disconnectHandler(){
-  console.log("disconnected :(");
-}
-
-function reconnectHandler(){
+namespace.module('game.socket', function(exports, require){
   
-}
-
-function reconnectingHandler(nextRetry){
+  var game = require('game.drawing')
   
-}
-
-function reconnect_failedHandler(){
+  socket = new io.Socket(null, {port: 3000, rememberTransport: false});
+  socket.connect();
   
-}
-
-function messageHandler(obj){
+  exports.socket = socket;
   
-}
+  var socket_events = {
+    'connect' : connectHandler
+   ,'disconnect' : disconnectHandler
+   ,'reconnect' : reconnectHandler
+   ,'reconnecting' : reconnectingHandler
+   ,'reconnect_failed' : reconnect_failedHandler
+   ,'message' : messageHandler
+  }
+  
+  Object.keys(socket_events).forEach( function(event, index, arr){
+    var f = socket_events[event];
+    if(!f) console.log("Cannot find " + fname + 'function. Please create it!' );
+    else socket.on(event,f);
+  })
+
+  function connectHandler(){
+    console.log("i've connected!!");
+  }
+
+  function disconnectHandler(){
+    console.log("disconnected :(");
+  }
+
+  function reconnectHandler(){
+
+  }
+
+  function reconnectingHandler(nextRetry){
+
+  }
+
+  function reconnect_failedHandler(){
+
+  }
+
+  function messageHandler(msg){
+
+    console.log("message received: " + msg);
+
+    // Only to be called whenever we connect as a new client
+    if(msg.type == 'onNewConnect'){  
+      // Synchronize everyone
+      game.setPlayers(msg.everyone);
+      game.setMe(msg.me.username);
+      game.init();   // Entry point to EaselJS
+    }
+
+    // A challenger appears!
+    else if(msg.type == 'newArrival'){
+      game.addNewPlayer(msg.player);
+    }
+
+    // Challenger changes position!
+    else if(msg.type == 'playerUpdate'){
+      console.log("hurray, server notified us!");
+      game.updatePlayer(msg);
+    }
+    
+  }
+  
+});
