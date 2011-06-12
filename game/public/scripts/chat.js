5f8497d1d2a7b04f93520c20184e0b55d2a57d18
Jamal Khan <jamal.khan91@gmail.com>
2011-06-12 21:40:06 +0100

    Basic chat working, the css is really fucked up will fix after Maciek's input

---
+++ game/public/scripts/chat.js
@@ -0,0 +1,29 @@
+function message(obj){
+  var el = document.createElement('li');
+  if ('announcement' in obj) el.innerHTML = '<em>' + esc(obj.announcement) + '</em>';
+  else if ('message' in obj) el.innerHTML = '<b>' + esc(obj.message[0]) + ':</b> ' + esc(obj.message[1]);
+  if( obj.message && window.console && console.log ) console.log(obj.message[0], obj.message[1]);
+  document.getElementById('chat').appendChild(el);
+  document.getElementById('chat').scrollTop = 1000000;
+}
+
+function send(){
+  var val = document.getElementById('chatInput').value;
+  socket.send({
+    type: "chat",
+    val: val  
+  });
+  message({message:['You', val]});
+  document.getElementById('chatInput').value = '';
+}
+      
+function esc(msg){
+  return msg.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+}
+
+function updateChat(msg){
+  var id = msg.message[0];
+  var text = msg.message[1];
+  message({message:[id, text]});
+}
+
