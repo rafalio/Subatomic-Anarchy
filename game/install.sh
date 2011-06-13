c6925faf5a2766c468813c92977b51943436ba38
Rafal Szymanski <rs@vlixter.com>
2011-06-13 14:11:00 +0100

    Blarg, added a simple install script.

---
+++ game/install.sh
@@ -0,0 +1,12 @@
+npm install express
+npm install mongoose
+npm install async
+npm install bcrypt
+npm install connect
+npm install jade
+npm install nodemailer
+npm install socket.io
+npm install underscore
+
+cd node_modules
+git clone git://github.com/radicality/forms.git
